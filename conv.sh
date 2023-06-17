44287a89fbb6b122fdb7661adfad69f673bb9351
6in <0hya6in@gmail.com>
2023-06-17 09:57:02 +0900

    zip処理追加

--- conv.sh
+++ conv.sh
@@ -3,6 +3,3 @@ SRCPATH=/root/work/input/sample.pptx
 docker-compose run office /usr/bin/soffice --nolockcheck --nologo --headless --norestore \
                  --language=ja --nofirststartwizard --convert-to pdf \
                  --outdir "${DSTPATH}" "${SRCPATH}"
-
-# PDFを１ページごとにPNGで出力
-pdftoppm -png ${DSTPATH}/sample.pdf output/sample.pdf
